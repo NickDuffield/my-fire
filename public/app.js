374437d0bc1138be1ac42e76f5e3bbc687bc34b3
Nick Duffield <hi.nickduffield@gmail.com>
2018-02-13 08:01:55 +0000

    firebase config added

---
+++ public/app.js
@@ -0,0 +1,21 @@
+
+(function(){
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyB2-brY9WU1yl18sqenEopoQeMLrSUw5hY",
+    authDomain: "my-fire-9da77.firebaseapp.com",
+    databaseURL: "https://my-fire-9da77.firebaseio.com",
+    projectId: "my-fire-9da77",
+    storageBucket: "my-fire-9da77.appspot.com",
+    messagingSenderId: "274167886619"
+  };
+  firebase.initializeApp(config);
+
+  //get elements
+  const preObject = Document.getElementById('object');
+
+  //create references
+  const dbRefObject = firebase.database().ref().child('object');
+
+}());

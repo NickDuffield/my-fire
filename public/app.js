7dc86ed9aa5f0ae02bc58abd40a5e10dba09e540
Nick Duffield <hi.nickduffield@gmail.com>
2018-02-16 07:45:02 +0000

    Create realistic marker data examples

--- public/app.js
+++ public/app.js
@@ -12,11 +12,64 @@
   };
   firebase.initializeApp(config);
 
+
+  //USEFUL PARTS
+
+  //connection to database
+  var mySpots = firebase.database().ref().child('spotsTest');
+
+  //data logged to console
+  mySpots.on('value', snap => console.log(snap.val()));
+
+
+/*
+  //MY TESTS
+
+  //connection to database
+  var mySpots = firebase.database().ref().child('spotsTest');
+  var spotList = mySpots.child('spot-1');
+
+  //get html element
+  var ulList = document.getElementById('list');
+  var myObject = document.getElementById('myElement'); //remove this
+
+  //data logged to console
+  mySpots.on('value', snap => console.log(snap.val()));
+
+  //intersting that 'null, 1' handles the level of indentation for children of the object
+  mySpots.on('value', snap => {
+    myElement.innerText = JSON.stringify(snap.val(), null, 1);
+  });
+
+  //append data to element when data is added to database
+  spotList.on('child_added', snap => {
+    var li = document.createElement('li');
+    li.innerText = snap.val();
+    li.id = snap.key; //may not need this
+    ulList.appendChild(li);
+  });
+
+  //remove element when data is removed from database
+  spotList.on('child_removed', snap => {
+    var liToRemove = document.getElementById(snap.key)
+    liToRemove.remove();
+  });
+
+  //update element when data us changed in database
+  spotList.on('child_changed', snap => {
+    var liChanged = document.getElementById(snap.key);
+    liChanged.innerText = snap.val();
+  });
+*/
+
+/*
+  //TRAINING EXAMPLES
+
   //get elements
   var myObject = document.getElementById('myElement');
   var ulList = document.getElementById('list');
 
-  //create references
+  //create database references
   var dbRef = firebase.database().ref().child('dataObject');
   var dbRefList = dbRef.child('home');
 
@@ -24,9 +77,6 @@
   dbRef.on('value', snap => {
     myElement.innerText = JSON.stringify(snap.val(), null, 3);
   });
-
-  //sync list changes - may be useful to me. everytime a spot is added it is then pinned to the map
-  //dbRefList.on('child_added', snap => console.log(snap.val()));
 
   dbRefList.on('child_added', snap => {
     var li = document.createElement('li');
@@ -45,7 +95,5 @@
     var liToRemove = document.getElementById(snap.key)
     liToRemove.remove();
   });
-
-
-
+*/
 }());
